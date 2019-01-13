648f08b120f3cba578b398b64073c0def037fa08
Eric x. sun <eric.x.sun@gmail.com>
2019-01-13 23:03:42 +0800

    Update linuxbrew.sh

--- linuxbrew.sh
+++ linuxbrew.sh
@@ -14,12 +14,12 @@ echo "Install linuxbrew"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 
 echo "Config"
-sed -i -e "/export PATH=\"\/home\/linuxbrew\/.linuxbrew\/bin:\$PATH\"/d" ~/.bashrc
-sed -i -e "/export MANPATH=\"\/home\/linuxbrew\/.linuxbrew\/share\/man:\$MANPATH\"/d" ~/.bashrc
-sed -i -e "/export INFOPATH=\"\/home\/linuxbrew\/.linuxbrew\/share\/info:\$INFOPATH\"/d" ~/.bashrc
+sed -i -e "/export PATH=\"/home/linuxbrew/.linuxbrew/bin:\$PATH\"/d" ~/.bashrc
+sed -i -e "/export MANPATH=\"/home/linuxbrew/.linuxbrew/share/man:\$MANPATH\"/d" ~/.bashrc
+sed -i -e "/export INFOPATH=\"/home/linuxbrew/.linuxbrew/share/info:\$INFOPATH\"/d" ~/.bashrc
 
-echo "export PATH=\"\/home\/linuxbrew\/.linuxbrew/bin:\$PATH\"" >> ~/.bashrc
-echo "export MANPATH=\"\/home\/linuxbrew\/.linuxbrew/share/man:\$MANPATH\"" >> ~/.bashrc
-echo "export INFOPATH=\"\/home\/linuxbrew\/.linuxbrew/share/info:\$INFOPATH\"" >> ~/.bashrc
+echo "export PATH=\"/home/linuxbrew/.linuxbrew/bin:\$PATH\"" >> ~/.bashrc
+echo "export MANPATH=\"/home/linuxbrew/.linuxbrew/share/man:\$MANPATH\"" >> ~/.bashrc
+echo "export INFOPATH=\"/home/linuxbrew/.linuxbrew/share/info:\$INFOPATH\"" >> ~/.bashrc
 
 source ~/.bashrc
